7098038d43a77ab40f340025c3679c35286803c4
Gabriel Breton <gabriel.breton@protonmail.com>
2018-03-05 00:41:11 +0100

    Update javadoc.

--- src/projet/pictionnary/breton/components/DrawingInfos.java
+++ src/projet/pictionnary/breton/components/DrawingInfos.java
@@ -4,23 +4,34 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
- *
+ * This class is used as a DTO to transfer a draw.
+ * 
  * @author Gabriel Breton - 43397
  */
 public class DrawingInfos implements Serializable {
     
     private final ArrayList<Point> listPositions;
     
-    
+    /**
+     * Creates a new DrawingInfos.
+     */
     public DrawingInfos() {
         listPositions = new ArrayList<>();
     }
     
+    /**
+     * Add a point of the draw to the list.
+     * @param p 
+     */
     public void add(Point p) {
         listPositions.add(new Point(p.getX(), p.getY(), p.getThickness(), 
                                       p.getColor(), p.isErase()));
     }
     
+    /**
+     * Gives the list of point.
+     * @return 
+     */
     public ArrayList<Point> getListPositions() {
 //        ArrayList<Point> copyListPos = new ArrayList<>();
 //        
