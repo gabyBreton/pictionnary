bbc2d877f9a8d43b75db080a95b46c9b2668134e
Gabriel Breton <gabriel.breton@protonmail.com>
2018-03-12 12:21:34 +0100

    Add Observer/Observable interfaces

---
+++ src/projet/pictionnary/breton/util/Observable.java
@@ -0,0 +1,23 @@
+package projet.pictionnary.breton.util;
+
+/**
+ * Interface to implement the observer/observable pattern.
+ *
+ * @author Gabriel Breton - 43397
+ */
+public interface Observable {
+
+    /**
+     * Add an observer in the list of observers.
+     *
+     * @param o the observer to add.
+     */
+    public void addObserver(Observer o);
+
+    /**
+     * Deletes an observer from the list of observers.
+     *
+     * @param o the observer to delete.
+     */
+    public void deleteObserver(Observer o);
+}
---
+++ src/projet/pictionnary/breton/util/Observer.java
@@ -0,0 +1,14 @@
+package projet.pictionnary.breton.util;
+
+/**
+ * Interface to implement the observer/observable pattern.
+ * 
+ * @author Gabriel Breton - 43397
+ */
+public interface Observer {
+    
+    /**
+     * Updates an observer.
+     */
+    public void update();      
+}
