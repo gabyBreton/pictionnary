5ecc187c31a001432dd70e64667791364e110984
Gabriel Breton <gabriel.breton@protonmail.com>
2018-04-05 00:40:17 +0200

    add fxml client connexion view

---
+++ src/projet/pictionnary/breton/client/view/ConnexionStageController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet.pictionnary.breton.client.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Gabriel Breton - 43397
+ */
+public class ConnexionStageController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
