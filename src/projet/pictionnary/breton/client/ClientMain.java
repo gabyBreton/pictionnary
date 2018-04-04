09a2534912dc6f5c2e1f41db6bca531081d3120c
Gabriel Breton <gabriel.breton@protonmail.com>
2018-04-05 00:59:41 +0200

    add the capacity to launch the client

---
+++ src/projet/pictionnary/breton/client/ClientMain.java
@@ -0,0 +1,33 @@
+package projet.pictionnary.breton.client;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Gabriel Breton - 43397
+ */
+public class ClientMain extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("view/ConnexionStage.fxml"));
+        
+        Scene scene = new Scene(root);
+       
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Pictionnary");
+        primaryStage.show();
+    }
+}
--- src/projet/pictionnary/breton/client/view/ConnexionStageController.java
+++ src/projet/pictionnary/breton/client/view/ConnexionStageController.java
@@ -1,13 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projet.pictionnary.breton.client.view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,6 +15,29 @@ import javafx.fxml.Initializable;
  */
 public class ConnexionStageController implements Initializable {
 
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField pseudoTfd;
+
+    @FXML
+    private TextField serverIpTfd;
+
+    @FXML
+    private TextField portNumberTfd;
+
+    @FXML
+    private Button connexionBtn;
+
+    @FXML
+    void connect(ActionEvent event) {
+
+    }
+    
     /**
      * Initializes the controller class.
      */
